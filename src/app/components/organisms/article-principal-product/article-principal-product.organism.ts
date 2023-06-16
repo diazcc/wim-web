6eba374d059dd9dfd72d7d4b0141962b703e8eed
Andres Diaz <diazccx@gmail.com>
2023-06-15 19:27:42 -0500

    Create component article, must create section not article

---
+++ src/app/components/organisms/article-principal-product/article-principal-product.organism.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-principal-product',
+  templateUrl: './article-principal-product.organism.html',
+  styleUrls: ['./article-principal-product.organism.scss']
+})
+export class ArticlePrincipalProductOrganism {
+
+}

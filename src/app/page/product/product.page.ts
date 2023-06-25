f062909f7fab957dec3ca4eb35a516eb059bfb64
Andres Diaz <diazccx@gmail.com>
2023-06-25 11:48:56 -0500

    order product template to page

--- src/app/page/product/product.page.ts
+++ src/app/page/product/product.page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Input,Renderer2, ChangeDetectorRef  } from '@angular/core';
+import { ProductServicesService } from 'src/app/services/product-services.service';
+import { map } from 'rxjs';
+import { collection, onSnapshot, DocumentSnapshot, doc } from 'firebase/firestore';
+import { Firestore } from '@angular/fire/firestore';
+import { NavigationExtras, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-product',
@@ -6,5 +12,168 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.page.scss']
 })
 export class ProductPage {
+  @Input() classMain = "";
+
+  @Input() idProductState : any;
+  @Input() idProduct : any;
+  @Input() dataSearch = {
+    classSearch : "hidde",
+    closeSearch : () =>{}
+   }
+  @Input() dataHeader = {
+    textTitle :"Infinity Industry",
+    urlIconMenu: "assets/icons/menu.svg",
+    classHeader :"header",
+    classContentHeader :"content-header",
+    classIconMenu :"icon-Menu",
+    classIconMenu2 :"classIconMenu",
+    classHeaderTitulo :"header-titulo",
+    clickHeader : () => {this.showMenu()},
+    clickSearch : () => {this.setSearch()},
+    dataNavBar : {
+      textOption1 : "Adidas",
+      textOption2 : "Nike",
+      textOption3 : "Quest",
+      textOption4 : "Puma",
+      classMenu : "menu--hidden",
+      classContentMenu : "content-menu",
+      classOptionMmenu : "option-menu"
+    }
+  }
+  @Input() dataViewProduct = {
+    name :"",
+    urlImg : "",
+    value : "",
+    description : ""
+  }
+
+
+
+  @Input() dataCardProduct = {
+    data : [
+      {
+        urlImgPrincipalProduct : "/assets/img/gorra-principal.jpg",
+        textTitle : "",
+        textDescription :"",
+        textValue : "",
+        clickProduct : () =>{}
+      }
+    ]
+  }
+
+  constructor(
+    private productServices : ProductServicesService,
+    private router : Router,
+    private elementRef: ElementRef,
+    private renderer : Renderer2,
+    private firestore: Firestore,
+    private cdr: ChangeDetectorRef
+  ){
+    const navigation = this.router.getCurrentNavigation();
+    this.idProductState = navigation?.extras.state as any;
+    this.idProduct = this.idProductState.idProduct;
+    console.log(this.idProduct);
+  }
+
+  ngOnInit(){
+    this.getProducts();
+    const prodRef = doc(this.firestore,"caps",this.idProduct);
+    const prod = onSnapshot(prodRef, (snap) => {
+      const dataSnap : any = snap.data();
+      console.log(dataSnap);
+      this.dataViewProduct = {
+          name :dataSnap.name,
+          urlImg : dataSnap.urlImg,
+          value : dataSnap.value,
+          description : dataSnap.description
+        }
+    });
+  }
+  setSearch(){
+    if (this.dataSearch.classSearch == "hidde") {
+      this.dataSearch = {
+        classSearch : "search",
+        closeSearch : () =>{this.closeSearch()}
+      }
+      this.dataHeader.classHeader = "hidde";
+    }else{
+      this.dataSearch = {
+        classSearch : "hidde",
+        closeSearch : () =>{this.closeSearch()}
+      }
+      this.dataHeader.classHeader = "header";
+    }
+  }
+  closeSearch(){
+    if (this.dataSearch.classSearch == "search") {
+      this.dataSearch.classSearch = "hidde";
+      this.dataHeader.classHeader = "header";
+    }else{
+      this.dataSearch.classSearch = "search";
+    }
+  }
+
+  showMenu(){
+
+    if (this.dataHeader.dataNavBar.classMenu=="menu--hidden") {
+    this.dataHeader.dataNavBar.classMenu="menu";
+    this.classMain = "filterBlur";
+    this.dataHeader.urlIconMenu = "assets/icons/close.svg"
+    this.dataHeader.classHeader = "focus";
+    }else{
+    this.dataHeader.dataNavBar.classMenu="menu--hidden";
+    this.classMain = "";
+    this.dataHeader.urlIconMenu = "assets/icons/menu.svg"
+    this.dataHeader.classHeader = "header";
+    }
+  }
+
+
+  getProducts(){
+    const prodRef = collection(this.firestore,'caps');
+    const prod = onSnapshot(prodRef, (snap)=>{
+      const product : any[] = [];
+      let arrayData : any = [];
+      snap.forEach(snapHijo =>{
+        product.push({
+          id: snapHijo.id,
+          ...snapHijo.data()
+        });
+      })
+      product.map((value : any) => {
+        const data =  {
+          id: value.id,
+          urlImgPrincipalProduct : value.urlImg,
+          textTitle : value.name,
+          textDescription :value.description,
+          textValue : value.value,
+          clickProduct :()=>{this.redirectProducts(value.id)}
+        }
+        arrayData.push(data);
+      });
+      this.setDataPrincipalProduct(arrayData);
+
+    });
+
+  }
+
+  setDataPrincipalProduct(responseData : any){
+    this.dataCardProduct.data = responseData;
+  }
+
+  redirectProducts(id : any){
+    window.scrollTo(0, 0);
+    const prodRef = doc(this.firestore,"caps",id);
+    const prod = onSnapshot(prodRef, (snap) => {
+      const dataSnap : any = snap.data();
+      console.log(dataSnap);
+      this.dataViewProduct = {
+          name :dataSnap.name,
+          urlImg : dataSnap.urlImg,
+          value : dataSnap.value,
+          description : dataSnap.description
+        }
+    });
+  }
 
 }
